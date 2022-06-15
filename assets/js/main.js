088be3d60cf3bb4831d8714fd32843910a68f993
Victor Santana Costa <victorsc.job@gmail.com>
2022-06-15 09:39:28 -0300

    feat: update project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,29 @@
 $(document).ready(function () {
-	$('.card_trend_slick_play__carousel').slick({
-		arrows: false,
-		dots: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		speed: 500,
-		fade: true,
-		cssEase: 'linear',
-	});
+	setTimeout(() => {
+		$('.card_trend_slick_play__carousel').slick({
+			arrows: false,
+			dots: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			speed: 500,
+			fade: true,
+			cssEase: 'linear',
+		});
 
-	$('.card_trend_slick_personal__carousel').slick({
-		arrows: false,
-		dots: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		speed: 500,
-		fade: true,
-		cssEase: 'linear',
-	});
+		$('.card_trend_slick_personal__carousel').slick({
+			arrows: false,
+			dots: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			speed: 500,
+			fade: true,
+			cssEase: 'linear',
+		});
+	}, 300);
 });
