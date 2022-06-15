6a3e33ab622842dabde0ab6651810332184bba3b
Victor Santana Costa <victorsc.job@gmail.com>
2022-06-14 21:59:59 -0300

    feat: add css and js plugins and update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,78 @@
-const { src, series, parallel, dest, watch } = require("gulp");
+const { src, series, parallel, dest, watch } = require('gulp');
 
-const gulp_clean_css = require("gulp-clean-css");
-const gulp_concat_css = require("gulp-concat-css");
-const gulp_concat_js = require("gulp-concat");
-const gulp_rename = require("gulp-rename");
-const gulp_sass = require("gulp-sass")(require("sass"));
-const gulp_sourcemaps = require("gulp-sourcemaps");
-const gulp_terser = require("gulp-terser");
-const gulp_uglify = require("gulp-uglify");
-const gulp_webp = require("gulp-webp");
+const gulp_clean_css = require('gulp-clean-css');
+const gulp_concat_css = require('gulp-concat-css');
+const gulp_concat_js = require('gulp-concat');
+const gulp_rename = require('gulp-rename');
+const gulp_sass = require('gulp-sass')(require('sass'));
+const gulp_sourcemaps = require('gulp-sourcemaps');
+const gulp_terser = require('gulp-terser');
+const gulp_uglify = require('gulp-uglify');
+const gulp_webp = require('gulp-webp');
 
-gulp_sass.compiler = require("node-sass");
+gulp_sass.compiler = require('node-sass');
 
 /******************************** TAREFAS *******************************/
 
 function img_webp_task() {
-  return src("./assets/img/**/**/**")
-    .pipe(gulp_webp())
-    .pipe(dest("./build/img/"));
+	return src('./assets/img/**/**/**')
+		.pipe(gulp_webp())
+		.pipe(dest('./build/img/'));
 }
 
 async function css_lib_task() {
-  const css_path_libs = require("./plugins-css.json");
-  return css_path_libs.map((item) => {
-    src(item.arquivos, { allowEmpty: true })
-      .pipe(gulp_concat_css("libs.min.css"))
-      .pipe(gulp_clean_css({ compatibility: "ie8" }))
-      .pipe(dest("./build/css"));
-  });
+	const css_path_libs = require('./plugins-css.json');
+	return css_path_libs.map((item) => {
+		src(item.arquivos, { allowEmpty: true })
+			.pipe(gulp_concat_css('libs.min.css'))
+			.pipe(gulp_clean_css({ compatibility: 'ie8' }))
+			.pipe(dest('./build/css'));
+	});
 }
 
 async function js_lib_task() {
-  const js_lib_path = require("./plugins-js.json");
-  return js_lib_path.map((item) => {
-    src(item.arquivos, { allowEmpty: true })
-      .pipe(gulp_concat_js("libs.min.js"))
-      .pipe(gulp_terser())
-      .pipe(gulp_uglify())
-      .pipe(dest("./build/js"));
-  });
+	const js_lib_path = require('./plugins-js.json');
+	return js_lib_path.map((item) => {
+		src(item.arquivos, { allowEmpty: true })
+			.pipe(gulp_concat_js('libs.min.js'))
+			.pipe(gulp_terser())
+			.pipe(gulp_uglify())
+			.pipe(dest('./build/js'));
+	});
 }
 
 function css_task() {
-  return src("./assets/sass/**/*.scss")
-    .pipe(gulp_sourcemaps.init())
-    .pipe(gulp_sass())
-    .pipe(gulp_rename({ suffix: ".min" }))
-    .pipe(gulp_sourcemaps.write("."))
-    .pipe(dest("./build/css"));
+	return src('./assets/sass/import.scss')
+		.pipe(gulp_sourcemaps.init())
+		.pipe(gulp_sass())
+		.pipe(gulp_rename({ suffix: '.min' }))
+		.pipe(gulp_sourcemaps.write('.'))
+		.pipe(dest('./build/css'));
 }
 
 function js_task() {
-  return src("./assets/js/**/*.js")
-    .pipe(gulp_sourcemaps.init())
-    .pipe(gulp_rename({ suffix: ".min" }))
-    .pipe(gulp_terser())
-    .pipe(gulp_sourcemaps.write("."))
-    .pipe(dest("./build/js"));
+	return src('./assets/js/**/*.js')
+		.pipe(gulp_sourcemaps.init())
+		.pipe(gulp_rename({ suffix: '.min' }))
+		.pipe(gulp_terser())
+		.pipe(gulp_sourcemaps.write('.'))
+		.pipe(dest('./build/js'));
 }
 
 function watch_css_task() {
-  watch(["./assets/sass/**/*.scss"], { interval: 1000 }, parallel(css_task));
+	watch(['./assets/sass/**/*.scss'], { interval: 1000 }, parallel(css_task));
 }
 
 function watch_js_task() {
-  watch(["./assets/js/**/*.js"], { interval: 1000 }, parallel(js_task));
+	watch(['./assets/js/**/*.js'], { interval: 1000 }, parallel(js_task));
 }
 
 function watch_task() {
-  watch(
-    ["./assets/sass/**/*.scss", "./assets/js/**/*.js"],
-    { interval: 1000 },
-    parallel(css_task, js_task)
-  );
+	watch(
+		['./assets/sass/**/*.scss', './assets/js/**/*.js'],
+		{ interval: 1000 },
+		parallel(css_task, js_task),
+	);
 }
 
 exports.img_webp_task = img_webp_task;
